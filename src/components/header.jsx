fae53ff1539324b4b8aa2ce2962be19ad734e770
Chizo Nwazuo <chizonwazuo@yahoo.com>
2022-08-08 12:31:04 +0000

    add logo 😋

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,22 @@
-import { Text, Box } from '@chakra-ui/react';
+import { Text, Box, Flex, HStack } from '@chakra-ui/react';
+import logo from '../assets/paradimemeals.svg';
 
 const Header = () => {
   return (
-    <Box py={10} borderBottomWidth="1px" borderColor="white" color="white">
-      <Text fontSize="4xl" py={0} fontWeight="bold">
-        Paradime Kitchen
-      </Text>
-      <Text>
+    <Box
+      pb={10}
+      pt={5}
+      borderBottomWidth="1px"
+      borderColor="white"
+      color="white"
+    >
+      <HStack spacing="12px">
+        <img src={logo} width={64} />
+        <Text fontSize="4xl" py={0} fontWeight="bold">
+          Prdm Kitchen
+        </Text>
+      </HStack>
+      <Text mt={2}>
         Tasty and healthy meals, powered by frictionless collaboration
       </Text>
     </Box>
