64d5d26e7c51cac3f6e47a3c0272f27843b24c88
Chizo Nwazuo <chizonwazuo@yahoo.com>
2022-08-08 16:29:43 +0000

    fix: scroll to top on route change

--- src/app.jsx
+++ src/app.jsx
@@ -3,15 +3,18 @@ import Layout from './components/layout';
 import Page404 from './routes/404';
 import Home from './routes/home';
 import Meal from './routes/meal';
+import ScrollToTop from './components/scrollToTop';
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/meal/:mealId" element={<Meal />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/meal/:mealId" element={<Meal />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </ScrollToTop>
     </Layout>
   );
 }
